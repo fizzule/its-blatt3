5c58ba9ba90e8d2177a66c0ca86254db9a693dc1
NilsBaumgartner <mllerpeter68@yahoo.de>
2016-12-04 22:08:41 +0100

    Update pa3_server.c

--- pa3_server.c
+++ pa3_server.c
@@ -1,7 +1,26 @@
+#include<stdio.h> //For standard things
+#include<stdlib.h>    //malloc
+#include<string.h>    //für strings
+
+char server_port[65536];
+int debug = 1;
 
 
 int main(int argc, char **argv){
+    int needed_arguments = 1; //programm self
+    needed_arguments++; //Server Port
  
+    if(argc==needed_arguments){
+        strncpy(server_port, argv[1], sizeof server_port);
+     
+        if(debug){
+            printf("Starting on Port: %s\n", server_port);
+        }
+                
+    }
+    else{
+        printf('usage: ./pa3_server SERVER_PORT \n');
+    }
   
     return 0;
 }
