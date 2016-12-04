fc540ec55bcc89e1372d3e75d1dd26026e041cb9
NilsBaumgartner <mllerpeter68@yahoo.de>
2016-12-04 21:37:59 +0100

    Update pa3_client.c

--- pa3_client.c
+++ pa3_client.c
@@ -2,10 +2,13 @@
 #include<stdlib.h>    //malloc
 #include<string.h>    //für strings
 
-server_adress[1024];
-server_port[1024];
-username[1024];
+server_adress[65536];
+server_port[65536];
+username[65536];
 char message[65536];
+
+int check_parameters();
+
  
 int main(int argc, char **argv){
     int needed_arguments = 1; //programm self
@@ -15,7 +18,14 @@ int main(int argc, char **argv){
     needed_arguments++; //From Here up the Message
  
     if(argc>=5){
-     printf("usage: ./pa3_client SERVER_ADRESS SERVER_PORT USERNAME 'Message to encypt' \n");
+       strncpy(server_adress, argv[1], sizeof server_adress);
+       strncpy(server_port, argv[2], sizeof server_port);
+       strncpy(username, argv[2], sizeof username);
+     
+       printf("Server Adress: %s\n", aserver_adress);
+       printf("Server Port: %s\n", aserver_port);
+       printf("Username: %s\n", username);
+       
     }
     else{
        printf("usage: ./pa3_client SERVER_ADRESS SERVER_PORT USERNAME 'Message to encypt' \n");
@@ -23,3 +33,4 @@ int main(int argc, char **argv){
  
     return 0;
 }
+
