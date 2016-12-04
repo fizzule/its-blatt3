67e37e9f2f8c40486df58ac7dd729b9bbdb77af2
NilsBaumgartner <mllerpeter68@yahoo.de>
2016-12-04 22:05:18 +0100

    Update pa3_client.c

--- pa3_client.c
+++ pa3_client.c
@@ -6,8 +6,12 @@ char server_adress[65536];
 char server_port[65536];
 char username[65536];
 char message[65536];
+int debug = 1;
+
+char private_key[65536];
 
 int check_parameters();
+int load_private_key();
 
  
 int main(int argc, char **argv){
@@ -17,18 +21,24 @@ int main(int argc, char **argv){
     needed_arguments++; //UserName
     needed_arguments++; //From Here up the Message
  
-    if(argc>=5){
-       strncpy(server_adress, argv[1], sizeof server_adress);
-       strncpy(server_port, argv[2], sizeof server_port);
-       strncpy(username, argv[3], sizeof username);
+    if(argc==needed_arguments){
+        strncpy(server_adress, argv[1], sizeof server_adress);
+        strncpy(server_port, argv[2], sizeof server_port);
+        strncpy(username, argv[3], sizeof username);
+        strncpy(message, argv[4], sizeof message);
      
-       printf("Server Adress: %s\n", server_adress);
-       printf("Server Port: %s\n", server_port);
-       printf("Username: %s\n", username);
-       
+        if(debug){
+            printf("Server Adress: %s\n", server_adress);
+            printf("Server Port: %s\n", server_port);
+            printf("Username: %s\n", username);
+            printf("Message: %s\n", message);
+        }
+        
+        load_private_key();
+        
     }
     else{
-       printf("usage: ./pa3_client SERVER_ADRESS SERVER_PORT USERNAME 'Message to encypt' \n");
+        printf('usage: ./pa3_client SERVER_ADRESS SERVER_PORT USERNAME "Message to encypt" \n');
     }
  
     return 0;
